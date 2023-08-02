2b0077321ea3def2393b143c79722a00974c3e64
Wissididom <30803034+Wissididom@users.noreply.github.com>
2023-08-02 08:31:46 +0200

    Implement INCLUDE_BOTS and add Prettier (#1)

--- setupCommands.js
+++ setupCommands.js
@@ -1,51 +1,53 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 import {
-	Client,
-	GatewayIntentBits,
-	Partials,
-	ApplicationCommandType,
-	ApplicationCommandOptionType
-} from 'discord.js';
+  Client,
+  GatewayIntentBits,
+  Partials,
+  ApplicationCommandType,
+  ApplicationCommandOptionType,
+} from "discord.js";
 
 dotenv.config();
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.DirectMessages
-	],
-	partials: [
-		Partials.User,
-		Partials.Channel,
-		Partials.GuildMember,
-		Partials.Message,
-		Partials.Reaction
-	]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+  ],
+  partials: [
+    Partials.User,
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.Message,
+    Partials.Reaction,
+  ],
 }); // Discord Object
 
 client.on("ready", () => {
-	console.log(`Logged in as ${client.user.tag}!`); // Logging
-	const registerObject = {
-		clone: {
-			name: 'clone',
-			description: 'Clone the current channels messages to another server using a webhook',
-			type: ApplicationCommandType.ChatInput,
-			options: [
-				{
-					name: 'webhook_url',
-					description: 'The webhook url to send the messages to (Don\' worry the command with only be visible to you)',
-					required: true,
-					type: ApplicationCommandOptionType.String
-				}
-			]
-		}
-	};
-	let promises = [];
-	promises.push(client.application?.commands?.create(registerObject['clone']));
-	Promise.all(promises).then(reolvedPromises => {
-		process.kill(process.pid, 'SIGTERM');
-	});
+  console.log(`Logged in as ${client.user.tag}!`); // Logging
+  const registerObject = {
+    clone: {
+      name: "clone",
+      description:
+        "Clone the current channels messages to another server using a webhook",
+      type: ApplicationCommandType.ChatInput,
+      options: [
+        {
+          name: "webhook_url",
+          description:
+            "The webhook url to send the messages to (Don' worry the command with only be visible to you)",
+          required: true,
+          type: ApplicationCommandOptionType.String,
+        },
+      ],
+    },
+  };
+  let promises = [];
+  promises.push(client.application?.commands?.create(registerObject["clone"]));
+  Promise.all(promises).then((reolvedPromises) => {
+    process.kill(process.pid, "SIGTERM");
+  });
 });
 
-client.login(process.env['TOKEN']);
+client.login(process.env["TOKEN"]);
